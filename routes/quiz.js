4ed5a5301d825b8e5359a912237a93ecbffddae6
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-03-23 11:52:11 -0400

    made function for shuffule and random part

--- routes/quiz.js
+++ routes/quiz.js
@@ -2,14 +2,33 @@ const express = require('express');
 const router = express.Router();
 const quizController = require('../controllers/quizController');
 
+router.get('/', (req, res) => {
 
 
 
 
+});
 
+let getwords = async () => {
 
+let Randompart = getRandompart();
 
+}
 
+let getRandompart = () => {
 
+let parts = ['noun', 'verb', 'adjective'];
+let randomIndex = Math.floor(Math.random() * parts.length);
+let randomPart = parts[randomIndex];
+return randomPart;
+}
+
+let shuffle = (array) => {
+    for (let i = 0; i < array.length-1; i--) {
+       let randomnumber = Math.floor(Math.random() * (i + 1));
+       [array[i], array[randomnumber]] = [array[randomnumber], array[i]];
+    }
+    return array;
+};
 
 module.exports = router;
