4cbf83266a04f74beb880562a97a67c70f7adea5
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-03-23 12:15:29 -0400

    generated choosen words array and sent to ejs

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,34 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const quizController = require('../controllers/quizController');
+const fs = require('fs').promises;
+const path = require('path');
 
-router.get('/', (req, res) => {
-
-
-
-
+router.get('/', async (req, res) => {
+    try {
+        const words = await getWords();
+        res.json(words);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error getting words');
+    }
 });
 
-let getwords = async () => {
+async function getWords() {
+    const randomPart = getRandomPart();
+    const filePath = path.join(__dirname, '../resources/allwords.txt');
+    const allWords = await fs.readFile(filePath, 'utf-8');
 
-let Randompart = getRandompart();
+    const wordsArray = allWords
+        .split('\n')
+        .filter(line => line.trim() !== '');
 
+    shuffle(wordsArray);
+
+    const choices = [];
+
+    while (choices.length < 5 && wordsArray.length > 0) {
+        const line = wordsArray.pop();
+        const [word, part, def] = line.split('\t');
+
+        if (part === randomPart) {
+            choices.push({
+                word,
+                part,
+                def
+            });
+        }
+    }
+
+    return choices;
 }
 
-let getRandompart = () => {
-
-let parts = ['noun', 'verb', 'adjective'];
-let randomIndex = Math.floor(Math.random() * parts.length);
-let randomPart = parts[randomIndex];
-return randomPart;
+function getRandomPart() {
+    const parts = ['noun', 'verb', 'adjective'];
+    const randomIndex = Math.floor(Math.random() * parts.length);
+    return parts[randomIndex];
 }
 
-let shuffle = (array) => {
-    for (let i = 0; i < array.length-1; i--) {
-       let randomnumber = Math.floor(Math.random() * (i + 1));
-       [array[i], array[randomnumber]] = [array[randomnumber], array[i]];
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const randomNumber = Math.floor(Math.random() * (i + 1));
+        [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
     }
     return array;
-};
+}
 
 module.exports = router;
