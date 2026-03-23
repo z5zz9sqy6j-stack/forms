e89b3087726026dceb74a395be0571952d547b7e
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-03-23 03:53:41 -0400

    added user router and page

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routes/users');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use('/users', userRouter);
 
-app.listen(3030);
+app.get('/solution', (req, res) => {
+  let num1 = parseInt(req.query.num1);
+  let num2 = parseInt(req.query.num2);
+  let solution = num1 + num2;
+
+  res.send(`<h2>${num1} + ${num2} = ${solution}</h2>`);
+});
+
+app.get('/submit', (req, res) => {
+  console.log('--- GET Request Received ---');
+  console.log('Query Parameters:', req.query);
+
+  Object.entries(req.query).forEach(([key, value]) => {
+    console.log(`  ${key}: ${value}`);
+  });
+
+  console.log('----------------------------');
+
+  res.send('<h2>GET request received!</h2><pre>' + JSON.stringify(req.query, null, 2) + '</pre>');
+});
+
+app.post('/submit', (req, res) => {
+  console.log('--- POST Request Received ---');
+  console.log('Body:', req.body);
+
+  Object.entries(req.body).forEach(([key, value]) => {
+    console.log(`  ${key}: ${value}`);
+  });
+
+  console.log('-----------------------------');
+
+  res.send('<h2>POST request received!</h2><pre>' + JSON.stringify(req.body, null, 2) + '</pre>');
+});
+
+app.listen(3030, () => {
+  console.log('Server running on http://localhost:3030');
+});
