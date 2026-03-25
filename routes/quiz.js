b1191062562b48a9c52590949dc9363cb382fcd2
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-03-25 00:56:49 -0400

    fixed quiz page and completed assignment(quiz page was not working fixed it with some help)

--- routes/quiz.js
+++ routes/quiz.js
@@ -5,35 +5,37 @@ const path = require('path');
 
 router.get('/', async (req, res) => {
     try {
-        let chosenWords = await getWords();
-        console.log('Chosen Words:', chosenWords);
-
+        const chosenWords = await getWords();
         res.render('quiz', { chosenWords });
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error getting words');
+        res.status(500).send('Error getting quiz words');
     }
 });
 
 async function getWords() {
-    let randomPart = getRandomPart();
-    let filePath = path.join(__dirname, '../resources/allwords.txt');
-    let allWords = await fs.readFile(filePath, 'utf-8');
+    const randomPart = getRandomPart();
+    const filePath = path.join(__dirname, '../resources/allwords.txt');
+    const allWords = await fs.readFile(filePath, 'utf-8');
 
-    let wordsArray = allWords
+    const wordsArray = allWords
         .split('\n')
         .filter(line => line.trim() !== '');
 
     shuffle(wordsArray);
 
-    let choices = [];
+    const choices = [];
 
     while (choices.length < 5 && wordsArray.length > 0) {
-        let line = wordsArray.pop();
-        let [word, part, def] = line.split('\t');
+        const line = wordsArray.pop();
+        const [word, part, def] = line.split('\t');
 
         if (part === randomPart) {
-            choices.push({ word, part, def });
+            choices.push({
+                word,
+                part,
+                def
+            });
         }
     }
 
@@ -41,14 +43,14 @@ async function getWords() {
 }
 
 function getRandomPart() {
-    let parts = ['noun', 'verb', 'adjective'];
-    let randomIndex = Math.floor(Math.random() * parts.length);
+    const parts = ['noun', 'verb', 'adjective'];
+    const randomIndex = Math.floor(Math.random() * parts.length);
     return parts[randomIndex];
 }
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        let randomNumber = Math.floor(Math.random() * (i + 1));
+        const randomNumber = Math.floor(Math.random() * (i + 1));
         [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
     }
     return array;
