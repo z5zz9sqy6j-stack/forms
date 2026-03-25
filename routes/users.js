1b3dcbe17d8cd477f191f0f5205bcde2f6c9047f
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-03-25 00:21:16 -0400

    added user form

--- routes/users.js
+++ routes/users.js
@@ -2,45 +2,88 @@ const express = require('express');
 const router = express.Router();
 
 const users = [
-    { name: 'George' },
-    { name: 'Jenil' }
+    {
+        firstName: 'George',
+        lastName: 'Smith',
+        gender: 'Male',
+        age: 30
+    },
+    {
+        firstName: 'Justyna',
+        lastName: 'Brown',
+        gender: 'Female',
+        age: 28
+    }
 ];
 
-router.route('/')
-    .get((req, res) => {
-        res.send('User List');
-    })
-    .post((req, res) => {
-        const firstName = req.body.firstName;
-        const isValid = firstName !== "";
+router.get('/', (req, res) => {
+    res.redirect('/users/list');
+});
 
-        if (isValid) {
-            console.log(`Adding user: ${firstName}`);
-            users.push({ name: firstName });
-            res.render('users/list', { users });
-        } else {
-            console.log('Error adding user');
-            res.render('users/new', { firstName: firstName });
-        }
-    });
+router.post('/', (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const gender = req.body.gender;
+    const age = req.body.age;
+
+    const isValid =
+        firstName !== '' &&
+        lastName !== '' &&
+        gender !== '' &&
+        age !== '';
+
+    if (isValid) {
+        users.push({
+            firstName: firstName,
+            lastName: lastName,
+            gender: gender,
+            age: age
+        });
+
+        res.redirect('/users/list');
+    } else {
+        res.render('users/new', {
+            firstName: firstName,
+            lastName: lastName,
+            gender: gender,
+            age: age
+        });
+    }
+});
 
 router.get('/list', (req, res) => {
-    res.render('users/list', { users });
+    res.render('users/list', { users: users });
 });
 
 router.get('/new', (req, res) => {
-    res.render('users/new', { firstName: 'Test' });
+    res.render('users/new', {
+        firstName: '',
+        lastName: '',
+        gender: '',
+        age: ''
+    });
 });
 
-router.route('/:id')
-    .get((req, res) => {
-        res.send(`Getting user data: ${req.params.id}`);
-    })
-    .delete((req, res) => {
-        res.send(`Deleting user data for id: ${req.params.id}`);
-    })
-    .put((req, res) => {
-        res.send(`Updating user data for id: ${req.params.id}`);
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id];
+
+    if (!user) {
+        return res.send('User not found');
+    }
+
+    res.render('users/show', {
+        user: user,
+        id: id
     });
+});
+
+router.delete('/:id', (req, res) => {
+    res.send(`Deleting user data for id: ${req.params.id}`);
+});
+
+router.put('/:id', (req, res) => {
+    res.send(`Updating user data for id: ${req.params.id}`);
+});
 
 module.exports = router;
